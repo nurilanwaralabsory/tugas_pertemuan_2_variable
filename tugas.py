2f44c5dbe6f0a59e68c3ecbcd497450a8a2f7dee
Nuril Anwar Al Absory <nurilanwaralabsory@gmail.com>
2024-10-06 17:01:53 +0700

    tugas pertemuan 2 - variable

---
+++ tugas.py
@@ -0,0 +1,36 @@
+# 1. Rumus menghitung luas persegi
+# luas = sisi x sisi
+sisi = 5
+luasPersegi = sisi * sisi
+print(luasPersegi)
+
+# 2. Rumus menghitung luas persegi panjang
+# luas = panjang x lebar
+panjang = 10
+lebar = 5
+luasPersegiPanjang = panjang * lebar
+print(luasPersegiPanjang)
+
+# 3. Rumus menghitung luas segitiga
+# luas = 1/2 x alas x tinggi
+alas = 10
+tinggi = 5
+luasSegitiga = 1/2 * alas * tinggi
+print(luasSegitiga)
+
+# 4. Rumus menghitung luas lingkaran
+# luas = phi x jari-jari x jari-jari
+phi = 3.14
+jari_jari = 7
+luasLingkaran = phi * jari_jari * jari_jari
+print(luasSegitiga)
+
+# 5. Perbandingan lebih besar dan lebih kecil dari luas persegi dan luas persegi panjang
+print(luasPersegi > luasPersegiPanjang)
+print(luasPersegi < luasPersegiPanjang)
+
+# 6. Perbandingan lebih besar dan lebih kecil dari luas segitiga dan luas lingkaran
+print(luasSegitiga > luasLingkaran)
+print(luasSegitiga < luasLingkaran)
+
+
